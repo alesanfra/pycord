fa6a7b52251f385c597904222749c540d0a4cd2f
Alessio Sanfratello <sanfra90@gmail.com>
2020-03-21 13:38:11 +0100

    Added travis CI configuration

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
+import os
+
 from setuptools import setup
+
+requirements = os.path.abspath(os.path.join(os.path.dirname(__file__), 'requirements.txt'))
+install_requires = []
+
+if os.path.isfile(requirements):
+    with open(requirements) as f:
+        install_requires = f.read().splitlines()
 
 setup(
     name='pycord',
@@ -6,10 +15,7 @@ setup(
     version='0.0.1',
     description='Convenient wrapper on FFmpeg',
     author='Alessio Sanfratello',
-    url='https://github.com/kkroening/ffmpeg-python',
+    url='https://github.com/alesanfra/pycord',
     # download_url=download_url,
-    install_requires=[
-        'ffmpeg-python',
-        'numpy'
-    ],
+    install_requires=install_requires
 )
