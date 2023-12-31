8a31d4a1ff63c7722a54050c130b78c9c44af9e5
Alessio Sanfratello <sanfra90@gmail.com>
2023-12-31 15:05:02 +0100

    multi processing

--- pycord/__init__.py
+++ pycord/__init__.py
@@ -1,3 +1,26 @@
-"""Convenient wrapper on top of FFmpeg"""
+"""Convenient wrapper on top of FFMpeg"""
 
 __version__ = "0.1.1"
+
+import multiprocessing as _mp
+
+_mp.set_start_method("spawn")
+
+
+def _decode(queue: _mp.Queue):
+    queue.put("hi")
+
+
+def read(timeout: float = 10.0):
+    queue = _mp.Queue()
+
+    process = _mp.Process(target=_decode, args=(queue,))
+    process.start()
+
+    while True:
+        frame = queue.get(block=True, timeout=timeout)
+        if frame is None:
+            break
+        yield frame
+
+    process.join()
